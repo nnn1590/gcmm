3be9af3e1f9f65032e1a7812b692e4bce6c37adf
nnn1590 <nnn1590@nnn1590.org>
2020-05-08 10:46:19 +0900

    freetype.c: ft2build.h -> freetype2/ft2build.h

--- source/freetype.c
+++ source/freetype.c
@@ -12,7 +12,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <ogc/lwp_watchdog.h>
-#include <ft2build.h>
+#include <freetype2/ft2build.h>
 #include FT_FREETYPE_H
 #include "bannerload.h"
 #include "freetype.h"
